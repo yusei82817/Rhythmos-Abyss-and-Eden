7162f980e947000362df4602004a2df3d0d0e449
yuu3232 <yusei82817@aol.com>
2026-09-18 20:09:48 +0900

    Add Halloween game script

---
+++ hwgame.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const topButton=document.getElementById('top-button');
+    if(topButton) topButton.addEventListener('click',()=>{ location.href='title.html'; });
+});
